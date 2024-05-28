c01c813ac14f68dd6bb0a91e9948efbb74e66a88
Evgeniy <boreykojenya@yandex.ru>
2024-05-28 23:09:51 +0300

    x

---
+++ web/src/routes/(auth)/+layout.svelte
@@ -0,0 +1,5 @@
+<div class="mx-auto card border-0 rounded-0" style="max-width: 37em; margin: 0px auto;">
+	<div class="card-body">
+		<slot />
+	</div>
+</div>
---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<nav
+	class="navbar navbar-expand-lg bg-body"
+	style="height: 54px; background-color: var(--bs-content-bg); border-bottom: var(--bs-border-width) solid var(--bs-content-border-color);"
+>
+	<div class="container-fluid">
+		<a class="navbar-brand" href="/"> ugc-go-svelte </a>
+
+		<a href="/login" class="btn btn-primary ms-auto">Login</a>
+	</div>
+</nav>
+
+<slot />
